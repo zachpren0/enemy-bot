e23598c43c0daec3a2d5bcdac239826d54929382
zachpren0 <77454945+zachpren0@users.noreply.github.com>
2023-09-21 14:26:24 -0700

    Update responses.py

--- responses.py
+++ responses.py
@@ -1,3 +1,13 @@
 
 def get_response(message: str) -> str:
+
+
+    message = message.lower()
+    
+    if message =="hello":
+        return "hello"
+    
+    if message.__contains__("dylan"):
+        return "dylan roast"
+    
     return "default message"
